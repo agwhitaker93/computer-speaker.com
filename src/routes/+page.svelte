a72ce6a165a150061ac0707348f73bcd29c98a2a
Andrew Whitaker <agwhitaker93@gmail.com>
2024-02-04 14:33:34 +0000

    formatting, remove unused image imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script>
- import Counter from './Counter.svelte'
- import welcome from '$lib/images/svelte-welcome.webp'
- import welcome_fallback from '$lib/images/svelte-welcome.png'
- import header from '$lib/images/header.png'
+  import Counter from './Counter.svelte'
+  import header from '$lib/images/header.png'
 </script>
 
 <svelte:head>
@@ -11,9 +9,7 @@
 </svelte:head>
 
 <section>
-  <h1>
-    Hello!
-  </h1>
+  <h1>Hello!</h1>
   <div class="header">
     <picture>
       <img src={header} alt="Header image" />
@@ -21,54 +17,65 @@
   </div>
 
   <h2>I'm Andrew!</h2>
+  <p>I write code for a living, and I have a great time doing it!</p>
   <p>
-    I write code for a living, and I have a great time doing it!
+    Most of the code I write these days is JavaScript for Cloudflare Workers and helper utilities at
+    work. I enjoy the flexibility JavaScript gives me for solving problems, letting me weave object
+    oriented and functional concepts together.
   </p>
   <p>
-    Most of the code I write these days is JavaScript for Cloudflare Workers and helper utilities at work. I enjoy the flexibility JavaScript gives me for solving problems, letting me weave object oriented and functional concepts together.
+    I occasionally get the chance to write Java and Groovy code too! I learned Java in university,
+    but Groovy is still relatively new to me. I especially like Groovy's capacity for writing
+    self-contained scripts, using Grape to pull dependencies in. One fun example of that is a script
+    I wrote to compile PlantUML diagrams, pulling the JAR they provide down with Grape.
   </p>
   <p>
-    I occasionally get the chance to write Java and Groovy code too! I learned Java in university, but Groovy is still relatively new to me. I especially like Groovy's capacity for writing self-contained scripts, using Grape to pull dependencies in. One fun example of that is a script I wrote to compile PlantUML diagrams, pulling the JAR they provide down with Grape.
+    In my spare time, I've recently been learning how to use C# to make mods for Vintage Story. I've
+    not spent enough time with C# to have formed much of an opinion on it so far, but I am enjoying
+    how Harmony lets you dynamically modify and augment C# code.
   </p>
   <p>
-    In my spare time, I've recently been learning how to use C# to make mods for Vintage Story. I've not spent enough time with C# to have formed much of an opinion on it so far, but I am enjoying how Harmony lets you dynamically modify and augment C# code.
+    I have a slightly neglected project tying JavaScript and Rust together, with Tauri, to allow
+    alternate keybindings for games that don't natively provide them (like DarkTide). I really like
+    the idea of Rust, but again, I haven't spent enough time with it to really form an opinion.
+    Tauri is cool though!
   </p>
   <p>
-    I have a slightly neglected project tying JavaScript and Rust together, with Tauri, to allow alternate keybindings for games that don't natively provide them (like DarkTide). I really like the idea of Rust, but again, I haven't spent enough time with it to really form an opinion. Tauri is cool though!
-  </p>
-  <p>
-    A few years ago, I began writing a utility for myself with Clojure and ClojureScript, to help figure out what I can make with the various resources I have in Old School RuneScape. I really enjoy Clojure's syntax, but often feel like I'm not quite smart enough to take advantage of the more advanced features it provides.
+    A few years ago, I began writing a utility for myself with Clojure and ClojureScript, to help
+    figure out what I can make with the various resources I have in Old School RuneScape. I really
+    enjoy Clojure's syntax, but often feel like I'm not quite smart enough to take advantage of the
+    more advanced features it provides.
   </p>
 
   <Counter />
 </section>
 
 <style>
- section {
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   align-items: center;
-   flex: 0.6;
- }
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
 
- h1 {
-   width: 100%;
- }
+  h1 {
+    width: 100%;
+  }
 
- .header {
-   display: block;
-   position: relative;
-   width: 100%;
-   height: 0;
-   padding: 0 0 calc(100% * 350 / 750) 0;
- }
+  .header {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding: 0 0 calc(100% * 350 / 750) 0;
+  }
 
- .header img {
-   position: absolute;
-   width: 100%;
-   height: 100%;
-   top: 0;
-   display: block;
- }
+  .header img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    display: block;
+  }
 </style>
